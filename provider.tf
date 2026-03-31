ecddd85991e2c79cf463712278b03e927fe8430c
leejieunnnn <zjylje520@gmail.com>
2026-03-31 15:19:46 +0900

    s3이름변경2

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
     backend "s3" {
-        bucket = "90022760icy-0331-2026"
+        bucket = "90022760icy-0331-2026-v2"
         key = "terraform/state.tfstate"
         region = "ap-northeast-2"
         #dynamodb_table = "terraform-lock"
